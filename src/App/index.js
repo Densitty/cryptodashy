5799ab37c69882a6337734ca0242fb63727fff0c
Densitty <dennis.brough@yandex.com>
2021-04-29 05:08:00 -0500

    add a font from google fonts and wrap the App component with AppLayout

--- src/App/index.js
+++ src/App/index.js
@@ -2,37 +2,13 @@ import Welcome from "./Welcome";
 import "./App.css";
 /* styled component */
 import styled, { css } from "styled-components";
-
-const MyButton = styled.div`
-  color: green;
-  font-size: 2rem;
-  display: inline-block;
-  padding: 1rem;
-  border: 1px solid #ef9393;
-  margin: 0.6rem;
-  /* passing a prop */
-  ${(props) =>
-    props.primary &&
-    css`
-      background: #260f4e;
-      color: #fff;
-    `}
-`;
-
-/* Extending functionality in styled-components */
-const TomatoButton = styled(MyButton)`
-  color: tomato;
-  border-color: tomato;
-`;
+import AppLayout from "./AppLayout";
 
 function App() {
   return (
-    <div className="App">
+    <AppLayout>
       <Welcome />
-      <MyButton>Click Me</MyButton>
-      <MyButton primary>Primary Button</MyButton>
-      <TomatoButton>Primary Button</TomatoButton>
-    </div>
+    </AppLayout>
   );
 }
 
