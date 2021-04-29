6012a4624e8e4889f765ec1ca89db325e88e3fb2
Densitty <dennis.brough@yandex.com>
2021-04-29 04:53:45 -0500

    playing with styled components for prep

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,37 @@
+import Welcome from "./Welcome";
 import "./App.css";
+/* styled component */
+import styled, { css } from "styled-components";
+
+const MyButton = styled.div`
+  color: green;
+  font-size: 2rem;
+  display: inline-block;
+  padding: 1rem;
+  border: 1px solid #ef9393;
+  margin: 0.6rem;
+  /* passing a prop */
+  ${(props) =>
+    props.primary &&
+    css`
+      background: #260f4e;
+      color: #fff;
+    `}
+`;
+
+/* Extending functionality in styled-components */
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
 
 function App() {
   return (
     <div className="App">
-      <h1>The CryptoDashy App</h1>
+      <Welcome />
+      <MyButton>Click Me</MyButton>
+      <MyButton primary>Primary Button</MyButton>
+      <TomatoButton>Primary Button</TomatoButton>
     </div>
   );
 }
